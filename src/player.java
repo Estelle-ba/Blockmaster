fec75e35dd6e29b44ee967195ac786a9a5509fb7
Estelle-ba <estelle.bandhavong@edu.esiee-it.fr>
2025-01-21 11:12:57 +0100

    Add class

---
+++ src/player.java
@@ -0,0 +1,16 @@
+public class player {
+    //Attributes
+    String [][] board= new String[11][10];
+    player()
+    {
+        System.out.println("Welcome to javaTpoint");
+    }
+    //Method
+    public void play() {
+    }
+    public static void main(String[] args){
+        //  creating an object using new keyword
+        player obj = new player();
+        obj.play();
+    }
+}
