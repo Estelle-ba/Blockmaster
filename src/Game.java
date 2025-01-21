69d2817b748115668c505cbf22a2ec7ca5645deb
Nayash <shayan.cahelo@edu.esiee-it.fr>
2025-01-21 10:52:29 +0100

    init & function create lab en screen

---
+++ src/Game.java
@@ -0,0 +1,57 @@
+public class Game
+{
+
+    public static void main (String[] args) {
+        char[][] lab = new char[11][10];
+        create_lab(lab, 11, 10);
+        screen(lab, 11, 10);
+
+    }
+
+    public static char[][] create_lab(char[][] lab, int length, int width) {
+        for (int i = 0; i < length; i++) { // fill maze
+            for (int j = 0; j < width; j++) {
+                lab[i][j] = '#';
+            }
+        }
+        return lab;
+    }
+
+    public static void screen(char[][] lab, int length, int width) {
+        // function show the maze
+        String RED = "\u001B[31m";
+        String RESET = "\u001B[0m";
+        String GREEN = "\u001B[32m";
+        String BLUE = "\033[34m";
+
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                if (lab[i][j] == 'S') {
+                    System.out.print(GREEN + Character.toString(lab[i][j]) + RESET + "  ");
+                }
+                else if (lab[i][j] == 'E') {
+                    System.out.print(RED + Character.toString(lab[i][j]) + RESET + "  ");
+                }
+                else if (lab[i][j] == 'X') {
+                    System.out.print(BLUE + Character.toString(lab[i][j]) + RESET + "  ");
+                }
+                else if (lab[i][j] == 'P') {
+                    System.out.print(BLUE + Character.toString(lab[i][j]) + RESET + "  ");
+                }
+                else if (i == 0 || i == length - 1 || j == 0 || j == width - 1) {
+                    if (lab[i][j] != 'S' && lab[i][j] != 'E') {
+                        System.out.print(Character.toString('#') + "  ");
+                    }
+                }
+                else {
+                    System.out.print(Character.toString(lab[i][j]) + "  ");
+                }
+
+
+            }
+            System.out.print('\n');
+
+        }
+    }
+}
