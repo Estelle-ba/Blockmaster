bd68c03b3e5ca58302cbfaacd370ce5c14567bb6
Estelle-ba <estelle.bandhavong@edu.esiee-it.fr>
2025-01-22 09:47:50 +0100

    Move some function to player

--- src/gameboard.java
+++ src/gameboard.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
 
-public class gameboard {
+public class gameboard{
     //Attributes
     String [][] board= new String[10][11];
     byte nb_player = 2;
+    int[] first_coords = new int[4];
     gameboard()
     {
-    }
-
-    //Methods
-    public void Create(player player1, player player2, player player3, player player4){
-        /**
-         * This function instantiate the gameboard with the player in
-         * Take player objects
-         */
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 board[i][j]= "_";
             }
         }
+    }
 
+    //Methods
+    public void Add_player(player player1, player player2){
+        /**
+         * This function instantiate the gameboard with the player in
+         * Take player objects
+         */
         player1.play();
         board[4][5] = player1.sign;
         player1.changePosition(4,5);
         player2.play();
         board[5][5] = player2.sign;
         player2.changePosition(5,5);
+        /*
         if(nb_player>2){
             player3.play();
             board[5][5] = player3.sign;
@@ -35,7 +36,7 @@ public class gameboard {
                 board[5][5] = player4.sign;
                 player4.changePosition(5,4);
             }
-        }
+        }*/
     }
 
 
@@ -54,39 +55,19 @@ public class gameboard {
     }
 
 
-    public void destruct(player player) {
+    public void destruct(player player, int x, int y) {
         /**
          * This function bloc a case
          */
         // take
-        System.out.println(player.name +", choose somewhere to destruct");
-        System.out.print("Position x : ");
-        Scanner sc = new Scanner(System.in);
-        Byte x=0;
-        try{
-            x = sc.nextByte();
-        }
-        catch(Exception e){
-            destruct(player);
-        }
-        System.out.print("Position y : ");
-        Scanner sca = new Scanner(System.in);
-        Byte y = 0;
-        try{
-            y = sca.nextByte();
-        }
-        catch(Exception e){
-            destruct(player);
-        }
-
 
         if(x<1 || x > board.length){
             System.out.println("Not in a valid position !");
-            destruct(player);
+            player.destruct(this);
         }
         else if(y<1 || y > board[0].length){
             System.out.println("Not in a valid position !");
-            destruct(player);
+            player.destruct(this);
         }
         else{
             board[y-1][x-1] = "H";
@@ -94,20 +75,11 @@ public class gameboard {
     }
 
 
-    public void movement_player(player player) {
-        /**
-         * This function move the player
-         */
-        System.out.println(player.name +", it's your turn to move !");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        if(input.equals("z") || input.equals("Z")){
-            if(player.position[1]-1 >= 0){
-                if(board[player.position[1]-1][player.position[0]] == "_") {
-                    board[player.position[1]][player.position[0]] = "_";
-                    player.position[1] -= 1;
-                    board[player.position[1]][player.position[0]] = player.sign;
-                }
+    public void movement_player(player player, String input) {
+        if(player.position[1]-1 >= 0){
+            if(board[player.position[1]-1][player.position[0]] == "_") {
+                board[player.position[1]][player.position[0]] = "_";
+                player.position[1] -= 1;
             }
         }
         else if(input.equals("q") || input.equals("Q")){
@@ -115,7 +87,6 @@ public class gameboard {
                 if(board[player.position[1]][player.position[0]-1] == "_") {
                     board[player.position[1]][player.position[0]] = "_";
                     player.position[0] -= 1;
-                    board[player.position[1]][player.position[0]] = player.sign;
                 }
             }
         }
@@ -124,36 +95,22 @@ public class gameboard {
                 if(board[player.position[1]+1][player.position[0]] == "_") {
                     board[player.position[1]][player.position[0]] = "_";
                     player.position[1] += 1;
-                    board[player.position[1]][player.position[0]] = player.sign;
                 }
             }
         }
-        else if(input.equals("d") || input.equals("D")){
+        else{
             if(player.position[0]+1 < board[0].length){
                 if(board[player.position[1]][player.position[0]+1] == "_") {
                     board[player.position[1]][player.position[0]] = "_";
                     player.position[0] += 1;
-                    board[player.position[1]][player.position[0]] = player.sign;
+
                 }
             }
         }
-        else if(input.equals("quit") || input.equals("QUIT")){
-            nb_player -= 1;
-        }
-        else{
-            movement_player(player);
-        }
+        board[player.position[1]][player.position[0]] = player.sign;
     }
 
-    public void play(player player) {
-        /**
-         * This function is the normal way to play
-         */
-        printBoard();
-        movement_player(player);
-        printBoard();
-        destruct(player);
-    }
+
 
 
 
@@ -172,20 +129,21 @@ public class gameboard {
         //  creating player objects
         player player_1 = new player();
         player player_2 = new player();
+        /*
         player player_3 = new player();
-        player player_4 = new player();
+        player player_4 = new player();*/
 
         //Change player attributes
         player_2.sign="X";
         player_2.name="Xname";
 
+        /*
+        obj.Create(player_1, player_2, player_3, player_4);*/
 
-        obj.Create(player_1, player_2, player_3, player_4);
-
-
+        obj.Add_player(player_1, player_2);
         while(obj.nb_player > 1) {
-            obj.play(player_1);
-            obj.play(player_2);
+            player_1.play(obj);
+            player_2.play(obj);
         }
     }
 }
--- src/player.java
+++ src/player.java
@@ -1,4 +1,6 @@
-public class player {
+import java.util.Scanner;
+
+public class player{
     //Attributes
     int [] position = new int[2];
     boolean playing = false;
@@ -15,5 +17,55 @@ public class player {
     public void changePosition(int x, int y) {
         position[0] = y;
         position[1] = x;
+    }
+
+    public void move(gameboard board) {
+        System.out.println(name +", it's your turn to move !");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        if (input.equals("z") || input.equals("q")||input.equals("s") || input.equals("d")) {
+            board.movement_player(this, input);
+        }
+        else if (input.equals("Z") || input.equals("Q")||input.equals("S") || input.equals("D")) {
+            board.movement_player(this, input);
+        }
+        else if(input.equals("quit") || input.equals("QUIT")){
+            board.nb_player -= 1;
+        }
+        else{
+            move(board);
+        }
+    }
+    public void destruct(gameboard board) {
+        System.out.println(name +", choose somewhere to destruct");
+        System.out.print("Position x : ");
+        Scanner sc = new Scanner(System.in);
+        Byte x=0;
+        try{
+            x = sc.nextByte();
+        }
+        catch(Exception e){
+            destruct(board);
+        }
+        System.out.print("Position y : ");
+        Scanner sca = new Scanner(System.in);
+        Byte y = 0;
+        try{
+            y = sca.nextByte();
+        }
+        catch(Exception e){
+            destruct(board);
+        }
+        board.destruct(this, x, y);
+    }
+
+    public void play(gameboard board) {
+        /**
+         * This function is the normal way to play
+         */
+        move(board);
+        board.printBoard();
+        destruct(board);
+        board.printBoard();
     }
 }
