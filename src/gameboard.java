deffdad8cb641c01bf7bba0633141243600b7767
Estelle-ba <estelle.bandhavong@edu.esiee-it.fr>
2025-01-22 13:21:38 +0100

    add 2 player with name

--- src/gameboard.java
+++ src/gameboard.java
@@ -2,44 +2,21 @@ import java.util.Scanner;
 
 public class gameboard{
     //Attributes
-    String [][] board= new String[10][11];
-    byte nb_player = 2;
+    char [][] board= new char[10][11];
+    byte nb_player = 0;
     int[] first_coords = new int[4];
+    char empty = '_';
+    char wall = 'H';
     gameboard()
     {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                board[i][j]= "_";
+                board[i][j]= empty;
             }
         }
     }
 
     //Methods
-    public void Add_player(player player1, player player2){
-        /**
-         * This function instantiate the gameboard with the player in
-         * Take player objects
-         */
-        player1.play();
-        board[4][5] = player1.sign;
-        player1.changePosition(4,5);
-        player2.play();
-        board[5][5] = player2.sign;
-        player2.changePosition(5,5);
-        /*
-        if(nb_player>2){
-            player3.play();
-            board[5][5] = player3.sign;
-            player3.changePosition(4,4);
-            if(nb_player==4){
-                player4.play();
-                board[5][5] = player4.sign;
-                player4.changePosition(5,4);
-            }
-        }*/
-    }
-
-
     public void printBoard() {
         /**
          *
@@ -61,47 +38,49 @@ public class gameboard{
          */
         // take
 
-        if(x<1 || x > board.length){
+        if(x<0 || x > board.length-1){
             System.out.println("Not in a valid position !");
             player.destruct(this);
         }
-        else if(y<1 || y > board[0].length){
+        else if(y<0 || y > board[0].length-1){
             System.out.println("Not in a valid position !");
             player.destruct(this);
         }
         else{
-            board[y-1][x-1] = "H";
+            board[y-1][x-1] = 'H';
         }
     }
 
 
     public void movement_player(player player, String input) {
-        if(player.position[1]-1 >= 0){
-            if(board[player.position[1]-1][player.position[0]] == "_") {
-                board[player.position[1]][player.position[0]] = "_";
-                player.position[1] -= 1;
+        if(input.equals("z") || input.equals("Z")) {
+            if (player.position[1] - 1 >= 0) {
+                if (board[player.position[1] - 1][player.position[0]] == empty) {
+                    board[player.position[1]][player.position[0]] = empty;
+                    player.position[1] -= 1;
+                }
             }
         }
         else if(input.equals("q") || input.equals("Q")){
             if(player.position[0]-1 >= 0){
-                if(board[player.position[1]][player.position[0]-1] == "_") {
-                    board[player.position[1]][player.position[0]] = "_";
+                if(board[player.position[1]][player.position[0]-1] == empty) {
+                    board[player.position[1]][player.position[0]] = empty;
                     player.position[0] -= 1;
                 }
             }
         }
         else if(input.equals("s") || input.equals("S")){
             if(player.position[1]+1 < board.length){
-                if(board[player.position[1]+1][player.position[0]] == "_") {
-                    board[player.position[1]][player.position[0]] = "_";
+                if(board[player.position[1]+1][player.position[0]] == empty) {
+                    board[player.position[1]][player.position[0]] = empty;
                     player.position[1] += 1;
                 }
             }
         }
         else{
             if(player.position[0]+1 < board[0].length){
-                if(board[player.position[1]][player.position[0]+1] == "_") {
-                    board[player.position[1]][player.position[0]] = "_";
+                if(board[player.position[1]][player.position[0]+1] == empty) {
+                    board[player.position[1]][player.position[0]] = empty;
                     player.position[0] += 1;
 
                 }
@@ -110,40 +89,4 @@ public class gameboard{
         board[player.position[1]][player.position[0]] = player.sign;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    //Ce qui devra être dans la page main
-    public static void main(String[] args){
-        //  creating an object gameboard
-        gameboard obj = new gameboard();
-
-        //  creating player objects
-        player player_1 = new player();
-        player player_2 = new player();
-        /*
-        player player_3 = new player();
-        player player_4 = new player();*/
-
-        //Change player attributes
-        player_2.sign="X";
-        player_2.name="Xname";
-
-        /*
-        obj.Create(player_1, player_2, player_3, player_4);*/
-
-        obj.Add_player(player_1, player_2);
-        while(obj.nb_player > 1) {
-            player_1.play(obj);
-            player_2.play(obj);
-        }
-    }
 }
--- src/player.java
+++ src/player.java
@@ -4,14 +4,19 @@ public class player{
     //Attributes
     int [] position = new int[2];
     boolean playing = false;
-    String sign = "Y";
+    char sign = 'Y';
     String name = "Yname";
     player()
     {
     }
     //Method
-    public void play() {
+    public void start_play(String new_name, char new_sign, int x, int y, gameboard board) {
         playing = true;
+        name = new_name;
+        sign = new_sign;
+        changePosition(x, y);
+        board.nb_player +=1;
+        board.board[y][x] = sign;
     }
 
     public void changePosition(int x, int y) {
@@ -63,9 +68,47 @@ public class player{
         /**
          * This function is the normal way to play
          */
-        move(board);
-        board.printBoard();
-        destruct(board);
-        board.printBoard();
+        if(board.nb_player > 1){
+            move(board);
+            board.printBoard();
+            destruct(board);
+            board.printBoard();
+        }
     }
+
+    public static boolean WallCollision(gameboard board, char[][] lab, int x, int y) {
+        int cpt = 0;
+
+        if (x == 0) {
+            cpt += 1;
+        }
+        else if (lab[x - 1][y] == board.wall) { // top
+            cpt += 1;
+        }
+
+        if (x == 10) {
+            cpt += 1;
+        }
+        else if (lab[x + 1][y] == board.wall) { // down
+            cpt += 1;
+        }
+
+        if (y == 0) {
+            cpt += 1;
+        }
+        else if (lab[x][y - 1] == board.wall) { // left
+            cpt += 1;
+        }
+
+        if (y == 9) {
+            cpt += 1;
+        }
+        else if (lab[x][y + 1] == board.wall) { // right
+            cpt += 1;
+        }
+
+
+        return cpt >= 4;
+    }
+
 }
